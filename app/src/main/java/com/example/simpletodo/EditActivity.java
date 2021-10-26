41b259d0dc0e4fd01c242d41a67f8b1dd9fc7ac1
stampedsteve <67400828+stampedsteve@users.noreply.github.com>
2021-10-25 21:41:06 -0400

    Initial commit

---
+++ app/src/main/java/com/example/simpletodo/EditActivity.java
@@ -0,0 +1,41 @@
+package com.example.simpletodo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class EditActivity extends AppCompatActivity {
+
+    EditText edItem;
+    Button btnSave;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+
+        edItem = findViewById(R.id.edItem);
+        btnSave = findViewById(R.id.btnSave);
+
+        getSupportActionBar().setTitle("Edit Title");
+
+        edItem.setText(getIntent().getStringExtra(MainActivity.KEY_ITEM_TEXT));
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra(MainActivity.KEY_ITEM_TEXT,edItem.getText().toString());
+                intent.putExtra(MainActivity.KEY_ITEM_POSITION, getIntent().getExtras().getInt(MainActivity.KEY_ITEM_POSITION));
+
+                setResult(RESULT_OK,intent);
+                finish();
+            }
+        });
+
+    }
+}
